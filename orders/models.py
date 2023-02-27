28d53630f98c8f52bf1aec91e68a9595ac0782f9
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-02-27 19:32:07 +0100

    update migration

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Type(models.Model):
@@ -7,8 +8,10 @@ class Type(models.Model):
 
 class Order(models.Model):
     title = models.CharField(max_length=255)
-    date = models.DateField()
+    date_delivery = models.DateField()
     quantity = models.IntegerField()
     delivery_address = models.CharField(max_length=255)
     # on_delete=models.CASCADE - if specified type of ths object will be deleted then all associated object with this type will be deleted
     type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    # default=timezone.now() - if use 'now()' as method - date time will be hardcoded during migration. To be more dunamic use as ref to method 'now()'
+    date_created = models.DateTimeField(default=timezone.now)
