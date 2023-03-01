7164651290333cbf210a720e4accf422f7a8114b
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-03-01 20:10:23 +0100

    urls in apps

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 
+
+#app_name - will be used as erf in HTML to use methods
+app_name = 'orders'
+
 # Endpoints of movies app must be specifieds in urlpatterns:
 # movies/
 # moview/1/details
 urlpatterns = [
-    path("", views.index, name="orders_index"),
+    path("", views.index, name=f"index"),
     #<> - to use parametr
-    path('<int:order_id>', views.details, name='order_detail')
+    path('<int:order_id>', views.details, name='detail')
 ]
